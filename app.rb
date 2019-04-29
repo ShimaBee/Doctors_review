072bd61e3b4f57f7b8a375113129d4b1e571bad6
島袋雄太 <mrs1122ani@gmail.com>
2019-04-29 14:45:30 +0900

    DB接続

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pg'
+
+connection = PG::connect(:host => "localhost", :user => "shimabukuroyuuta", :dbname => "review_doctors", :port => 5432)
 
 get '/' do
+  @result = connection.exec("SELECT * FROM users")
   erb :index
 end
 
 get '/top' do
     erb :top
-  end
-  
+end
